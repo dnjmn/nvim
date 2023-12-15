a670786215ff4f36be78601a2dde7aeb0bae70f6
dhananjaymeena <dhananjay.meena@ibm.com>
2023-12-15 16:25:51 +0530

    telescope merge fix

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -6,14 +6,25 @@ local M = {
   cmd = "Telescope",
 }
 
-<<<<<<< HEAD
-local actions = require "telescope.actions"
-local builtin = require "telescope.builtin"
-=======
 function M.config()
   local icons = require "user.icons"
   local actions = require "telescope.actions"
->>>>>>> master
+  local builtin = require "telescope.builtin"
+
+  vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[] Find existing buffers" })
+  vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+  vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+  vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "[G]it [F]iles" })
+  vim.keymap.set("n", "<leader>th", builtin.help_tags, { desc = "[T]elescope [H]elp" })
+  vim.keymap.set("n", "<leader>ss", builtin.grep_string, { desc = "[S]earch [String]" })
+  vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+  vim.keymap.set("n", "<leader>sD", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+  vim.keymap.set(
+    "n",
+    "<leader>sd",
+    "<cmd>lua require('telescope.builtin').diagnostics({severity = vim.diagnostic.severity.ERROR})<CR>",
+    {}
+  )
 
   require("telescope").setup {
     defaults = {
@@ -123,15 +134,5 @@ function M.config()
     },
   }
 end
-
-vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[] Find existing buffers' })
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = '[G]it [F]iles' })
-vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = '[T]elescope [H]elp' })
-vim.keymap.set('n', '<leader>ss', builtin.grep_string, { desc = '[S]earch [String]' })
-vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sD', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sd', '<cmd>lua require(\'telescope.builtin\').diagnostics({severity = vim.diagnostic.severity.ERROR})<CR>', {})
 
 return M
